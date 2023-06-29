08079c25f500f383396f1aa37b4ccbde6530bf03
austine <austinndauwa@gmail.com>
2023-06-29 19:54:02 +0300

    change

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import catchAsync from './catchAsync';
+
 import axios from "axios";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded())
 app.use(cors())
+const catchAsync = require("../utils/catchAsync");
 
 
 
