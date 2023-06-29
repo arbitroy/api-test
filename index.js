87e02a463710a59c3e2f74718b85d9fcde65ebc9
asenawritescode <samasena2001@gmail.com>
2023-06-29 16:04:43 +0300

    Done

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.post('/slade', async (req, res) => {
 
         fetch(url, { method: "GET", headers })
             .then((data) => {
-                console.log(data)
+                console.log("api res",data)
                 res.json(data)
             })
     } else {
