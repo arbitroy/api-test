e91953604fbe6fced6404eb36c81dd8a21d3856a
asenawritescode <samasena2001@gmail.com>
2023-06-29 17:13:08 +0300

    Done

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded())
-app.use(cors("*"))
-
+app.use(cors())
+app.options('*', cors())
 let test = ""
 async function makeRequest(endpoint, type, payload, headers) {
     // switch between the type of request
