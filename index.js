e3ec41819eac6a280ebc6b70d8819084d2786177
asenawritescode <samasena2001@gmail.com>
2023-06-29 12:51:36 +0300

    test 10

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import cors from 'cors':
+
 const app = express();
+app.use(express.json());
+app.use(cors())
 
 async function makeRequest(endpoint, type, payload, headers) {
     // switch between the type of request
