28bcd246f21483e77ab6243cfa35caf6b11bae5b
asenawritescode <samasena2001@gmail.com>
2023-06-29 12:05:18 +0300

    test 5

--- index.js
+++ index.js
@@ -20,25 +20,7 @@ app.post('/ussd', (req, res) => {
 
     let response = '';
 
-    if (text == '') {
-        // This is the first request. Note how we start the response with CON
-        response = `CON What would you like to check
-        1. My account
-        2. My phone number`;
-    } else if ( text == '1') {
-        // Business logic for first level response
-        response = `CON Choose account information you want to view
-        1. Account number`;
-    } else if ( text == '2') {
-        // Business logic for first level response
-        // This is a terminal request. Note how we start the response with END
-        response = `END Your phone number is ${phoneNumber}`;
-    } else if ( text == '1*1') {
-        // This is a second level response where the user selected 1 in the first instance
-        const accountNumber = 'ACC100101';
-        // This is a terminal request. Note how we start the response with END
-        response = `END Your account number is ${accountNumber}`;
-    }
+    console.log(`This is ${sessionId} ${serviceCode} ${phoneNumber} ${text}`)
 
     // Send the response back to the API
     res.set('Content-Type: text/plain');
