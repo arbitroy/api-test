8e6859e2455517cf666a0ea071e368073a9a1ee5
asenawritescode <samasena2001@gmail.com>
2023-06-29 17:08:53 +0300

    Done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded())
-app.use(cors())
+app.use(cors("*"))
 
 let test = ""
 async function makeRequest(endpoint, type, payload, headers) {
